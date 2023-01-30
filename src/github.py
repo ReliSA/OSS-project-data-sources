737082fd739cce2cea95603f7268e0ed7fdb12cb
Jan Černogurský <91881532+Cernojan@users.noreply.github.com>
2023-01-31 00:41:58 +0100

    added first version of github updated config

--- src/github.py
+++ src/github.py
@@ -7,8 +7,11 @@ This module is the GitHub scraper.
 
 __all__ = ['scrape']
 
-
-# library imports here
+import time
+import sys
+import json
+import requests
+from lxml import html
 
 import data
 
@@ -16,6 +19,33 @@ import data
 # global config, to be provided from above
 _cfg: dict = {}
 
+# URLs for API access
+# /repos/{owner}/{repo}
+base_api_url = "https://api.github.com/"
+issues_api_url = "https://api.github.com/search/issues?q=repo:{0[owner]}/{0[repo-name]}+type:issue"
+open_issues_api_url = "https://api.github.com/search/issues?q=repo:{0[owner]}/{0[repo-name]}+type:issue+state:open"
+closed_issues_api_url = "https://api.github.com/search/issues?q=repo:{0[owner]}/{0[repo-name]}+type:issue+state:closed"
+members_api_url = "repositories/{0[id]}/members"
+contributors_api_url = "https://api.github.com/search/issues?q=repo:{0[owner]}/{0[repo-name]}+type:contributors"
+
+projects_api_url = "search/repositories?q=page=1&per_page={0[ProjectsPerPage]}&last_activity_after={0[DateLimit]}&order_by=id&sort=asc&"
+merge_requests_api_url = "projects/{0[id]}/merge_requests"
+releases_api_url = "projects/{0[id]}/releases"
+
+# URLs of web pages for scraping
+issues_url = "/issues"
+members_url = "-/project_members"
+contributors_url = "-/graphs/master"
+merge_requests_url = "-/merge_requests"
+
+# caching of things obtained just before
+_cache : dict = {
+    "api_url": None, "api_json": None,
+    "scraped_url": None, "scraped_doc": None
+}
+
+# file with mock project data, to use instead of API calls for testing
+_ApiMockDataFilePath = ""
 
 def scrape(cfg: dict):
     """ 
@@ -36,6 +66,193 @@ def read_projects(use_issue_num_limit: bool = True, use_only_starred: bool = Tru
     """
 
     global _cfg
-    
-    # TODO implement the scraping here
-    pass
+
+    url: str = None
+
+    json_data = None
+    links: dict = None  # the digested "Link" header of a 'requests' response
+
+    # setting the parameters
+    # params = {'q': 'language:python', 'sort': 'stars', 'order': 'desc'}
+
+    print("---------- github.com scraper started ----------", file=sys.stderr)
+    for page in range(int(_cfg['PageLimit'])):
+
+        if (links == None):
+            url = base_api_url + projects_api_url.format(_cfg)
+        else:
+            # pagination: https://docs.gitlab.com/ee/api/#keyset-based-pagination
+            if ('next' in links):
+                url = links['next']['url']
+            else:
+                break
+
+        print('---------- getting page ' + str(page + 1) + ' in ' + _cfg[
+            'PageLimit'] + ' of project data from url ' + url, file=sys.stderr)
+        (json_data, links) = get_json(url, _cfg['GitHubAccessToken'])  # , use_mock = True)
+
+        #print('got json, length:' + str(len(json_data)))
+        #print('got links: ' + str(links))
+
+        for item in json_data['items']:
+            print('parsing prj ' + str(item['id']), file=sys.stderr)
+            prj = fill_project(item)
+
+            # if (use_issue_num_limit and prj["issues-all"] < _cfg['LimitIssues']):
+            #     continue
+            # if (use_only_starred and prj["stars"] < _cfg['LimitStars']):
+            #     continue
+
+            data.add_prj(prj)
+            time.sleep(int(_cfg['SleepTime']))  # give the API a break
+
+    print("---------- github.com scraper finished. ----------", file=sys.stderr)
+
+
+def fill_project(obj: dict):
+    """
+    Gets project structure from `obj` JSON data and web page contents.
+    """
+
+    global _cfg
+
+    p = dict(data.PROJECT)
+    p['id'] = obj['id']
+    p['owner'] = obj['owner']['login']
+    p['repo-name'] = obj['name']
+    p['repo-url'] = obj['url']
+    p['url'] = obj['html_url']
+    p['created-at'] = obj['created_at']  # datetime strings
+    p['last-active-at'] = obj['updated_at']  # (convert do timestamp?)
+
+    if 'forks_count' in obj:
+        p['forks'] = obj['forks_count']
+    if 'license' in obj and obj['license']:
+        p['license'] = obj['license']['name']
+
+    # get project statistics
+
+    p['commits'] = get_scraped_value(p['url'], _cfg['GitHubCommitsCountXPath'])
+    p['releases'] = get_scraped_value(p['url'], _cfg['GitHubReleasesCountXPath'])
+
+    # make sure there really is not a license listed for the project
+    if (p['license'] == ""):
+        p['license'] = get_scraped_value(p['url'], _cfg['GitHubLicenseXPath'])
+
+    # "stars" are not in project data
+    p['stars'] = get_scraped_value(p['url'], _cfg['GitHubStarsCountXPath'])
+    # just to be sure, in case project data lacked this for some reason
+    if (p['forks'] == -1):
+        # print('no forks in data, scraping', file=sys.stderr)
+        p['forks'] = get_scraped_value(p['url'], _cfg['GitHubForksCountXPath'])
+
+
+    p['issues-all'] = get_api_value(issues_api_url.format(p), _cfg['GitHubAccessToken'])
+
+    # ^^^ constructs the API URL from the template strings and project's ID read above
+    p['issues-open'] = get_api_value(open_issues_api_url.format(p), _cfg['GitHubAccessToken'])
+    p['issues-closed'] = get_api_value(closed_issues_api_url.format(p), _cfg['GitHubAccessToken'])
+    p['contributors'] = get_api_value(contributors_api_url.format(p), _cfg['GitHubAccessToken'])
+
+    return p
+
+
+def get_api_value(url: str, token: str):
+    """
+    Query the data obtained from API using JSONPath expressions.
+    Returns the first value matching the given query.
+    """
+
+
+    global _cache
+
+    #print("apiquery: using url: " + url + " query: ")
+    # use cached value when scraping from the same URL
+    if (url == _cache['api_url']):
+        # print("apiquery: using cached response for url: " + url)
+        json_data = _cache['api_json']
+    else:
+        # print("apiquery: fresh request to url:" + url)
+        (json_data, _) = get_json(url, token)
+
+    total_count = json_data['total_count']
+
+    _cache['api_url'] = url
+    _cache['api_json'] = json_data
+
+    # print("apiquery: obtained value: " + str(res))
+    return total_count
+
+
+def get_scraped_value(url: str, xpathq: str):
+    """
+    Obtain a value from the HTML page at the given url,
+    as the inner text of element determined by the XPath query string.
+    Uses cached page if the 'url' is the same as when called previously.
+    """
+
+    global _cache
+
+    # use cached value when scraping from the same URL
+    if (url == _cache['scraped_url']):
+        # print("scraping: using cached response for url: " + url)
+        doc = _cache['scraped_doc']
+    else:
+        # print("scraping: fresh request to url:" + url)
+        req = requests.get(url)
+        if (req.status_code != 200):
+            return None
+        doc = html.document_fromstring(req.text)
+
+    e = doc.xpath(xpathq)
+    #print("scraping: obtained html element: " + str(e) + " for xpath: " + xpathq + " in url: " + url)
+
+    if (e):
+        res = e[0].text.strip()
+    else:
+        res = None
+
+    _cache['scraped_url'] = url
+    _cache['scraped_doc'] = doc
+
+    # print("scraping: obtained value: " + str(res))
+    return res
+
+
+def get_json(url: str, token: str, *, use_mock=False):
+    """
+    Gets data on projects from gitlab API.
+    Returns a tuple (parsed JSON payload, dictionary-of-links from Link header)
+    """
+
+    if (use_mock):
+        print("get_json: using mock project data for testing", file=sys.stderr)
+        return (read_json(), {})
+
+    json_data: str = ""
+
+    # print("get_json: from url " + url)
+    req = requests.get(url, headers={"Accept": "application/json", "Authorization": "Bearer %(GitHubAccessToken)s" % {'GitHubAccessToken': token}})
+
+    if (req.status_code != 200):
+        print("request doesn't equal to 200")
+        return ([], {})
+    json_data = req.json()
+    links = req.links
+
+    # print("get_json: headers in response: " + str(req.headers))
+    # print("get_json: links in response: " + str(req.links))
+    return (json_data, links)
+
+
+def read_json():
+    """
+    Returns mock data to test functionality.
+    """
+
+    file = _ApiMockDataFilePath
+    with open(file, "r") as f:
+        json_data = json.load(f)
+
+    return json_data
+
